02a7510737b7eabb5c0ffdcfd84f6c8f5d96cdd6
Thierno88 <82781367+Thierno88@users.noreply.github.com>
2024-02-01 20:45:24 -0300

    Add files via upload

---
+++ src/controller/testeFinal.java
@@ -0,0 +1,10 @@
+package controller;
+
+import view.IniciaInterface;
+import view.PlayersInfo;
+
+public class testeFinal {
+    public static void main(String[] args) {
+        ControladorJogo jogo = new ControladorJogo();
+    }
+}
